f7151e1821efea75cbbd1c88d423e09d62dd0277
seminozhenko.ss <grey_wolf.88@mail.ru>
2020-07-14 15:51:32 +0300

    Цикл по нескольким файлам

--- Test/Program.cs
+++ Test/Program.cs
@@ -14,111 +14,115 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            const string fileName = @"D:\GoogleDrive\Roslesinforg\Дела\2020.07.14 - Ц\ОСВ 205.31.xlsx";
+            //const string fileName = @"D:\GoogleDrive\Roslesinforg\Дела\2020.07.14 - Ц\ОСВ 205.31.xlsx";
 
-            string[] paths = { 
+            string[] paths = {
                 @"D:\GoogleDrive\Roslesinforg\Дела\2020.07.14 - Ц\ОСВ 205.31.xlsx",
-                @"D:\GoogleDrive\Roslesinforg\Дела\2020.07.14 - Ц\ОСВ 209.34.xlsx"
-            }
+                @"D:\GoogleDrive\Roslesinforg\Дела\2020.07.14 - Ц\ОСВ 209.34.xlsx",
+            };
 
             DataTable dt = new DataTable();
 
             dt.Clear();
             dt.Columns.Add("ОСВ");
-
-            DataColumn colString_3 = new DataColumn("КФО");
-            colString_3.DataType = System.Type.GetType("System.Int32");
-            dt.Columns.Add(colString_3);
-
+            dt.Columns.Add("КФО", System.Type.GetType("System.Int32"));
             dt.Columns.Add("Контрагент");
             dt.Columns.Add("Договор");
-
-            DataColumn colString = new DataColumn("Дебет");
-            colString.DataType = System.Type.GetType("System.Double");
-            dt.Columns.Add(colString);
-
-            DataColumn colString_2 = new DataColumn("Кредит");
-            colString_2.DataType = System.Type.GetType("System.Double");
-            dt.Columns.Add(colString_2);
+            dt.Columns.Add("Дебет", System.Type.GetType("System.Double"));
+            dt.Columns.Add("Кредит", System.Type.GetType("System.Double"));
 
             bool ReadMode = false;
 
-            try
+
+            foreach (var wb_path in paths)
             {
-                using (var excelWorkbook = new XLWorkbook(fileName))
+                try
                 {
-                    var nonEmptyDataRows = excelWorkbook.Worksheet(1).RowsUsed();
-
-                    int n = 0;
-
-
-                    string OSV, KFO, partner, contract, debet, kredit;
-                    OSV = "";
-                    KFO = "";
-                    partner = "";
-
-                    foreach (var dataRow in nonEmptyDataRows)
+                    using (var excelWorkbook = new XLWorkbook(wb_path))
                     {
+                        var nonEmptyDataRows = excelWorkbook.Worksheet(1).RowsUsed();
 
-                        var cell = dataRow.Cell(1);
+                        int n = 0;
 
-                        if (cell.Value.ToString() == "Итого")
+
+                        string OSV, KFO, partner, contract, debet, kredit;
+                        OSV = "";
+                        KFO = "";
+                        partner = "";
+
+                        foreach (var dataRow in nonEmptyDataRows)
                         {
-                            break;
-                        }
 
-                        if (cell.Value.ToString() == "Договоры")
-                        {
-                            ReadMode = true;
-                            continue;
-                        }
+                            var cell = dataRow.Cell(1);
 
-                        if (ReadMode == true) {
-
-                            switch (cell.Style.Alignment.Indent)
+                            if (cell.Value.ToString() == "Итого")
                             {
-                                case 0:
-                                    OSV = cell.Value.ToString();
-                                    break;
-                                case 2:
-                                    KFO = cell.Value.ToString();
-                                    break;
-                                case 4:
-                                    partner = cell.Value.ToString();
-                                    break;
-                                case 6:
-                                    contract = cell.Value.ToString();
-                                    debet = dataRow.Cell(19).Value.ToString();
-                                    kredit = dataRow.Cell(21).Value.ToString();
+                                break;
+                            }
 
-                                    DataRow _row = dt.NewRow();
+                            if (cell.Value.ToString() == "Договоры")
+                            {
+                                ReadMode = true;
+                                continue;
+                            }
 
-                                    _row["ОСВ"] = OSV;
-                                    _row["КФО"] = KFO;
-                                    _row["Контрагент"] = partner;
-                                    _row["Договор"] = contract;
-                                    _row["Дебет"] = debet == "" ? 0 : Convert.ToDouble(debet.Replace(".", ","));
-                                    _row["Кредит"] = kredit == "" ? 0 : Convert.ToDouble(kredit.Replace(".", ","));
+                            if (ReadMode == true)
+                            {
 
-                                    dt.Rows.Add(_row);
+                                switch (cell.Style.Alignment.Indent)
+                                {
+                                    case 0:
+                                        OSV = cell.Value.ToString();
+                                        break;
+                                    case 2:
+                                        KFO = cell.Value.ToString();
+                                        break;
+                                    case 4:
+                                        partner = cell.Value.ToString();
+                                        break;
+                                    case 6:
+                                        contract = cell.Value.ToString();
+                                        debet = dataRow.Cell(19).Value.ToString();
+                                        kredit = dataRow.Cell(21).Value.ToString();
 
-                                    Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", OSV, KFO, partner, contract, debet, kredit);
+                                        if (debet != "" || kredit != "")
+                                        {
+                                            DataRow _row = dt.NewRow();
 
-                                    n += 1;
+                                            _row["ОСВ"] = OSV;
+                                            _row["КФО"] = KFO;
+                                            _row["Контрагент"] = partner;
+                                            _row["Договор"] = contract;
+                                            _row["Дебет"] = debet == "" ? 0 : Convert.ToDouble(debet.Replace(".", ","));
+                                            _row["Кредит"] = kredit == "" ? 0 : Convert.ToDouble(kredit.Replace(".", ","));
 
-                                    break;
-                            }                         
+                                            dt.Rows.Add(_row);
+
+                                            //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", OSV, KFO, partner, contract, debet, kredit);
+                                        }
+
+                                        n += 1;
+
+                                        break;
+                                }
+                            }
                         }
                     }
                 }
-            }
 
 
-            catch (System.IO.IOException e)
-            {
-                Console.WriteLine("{0} Exception caught!", e);
-                Console.ReadKey();
+                catch (System.IO.IOException e)
+                {
+                    //Console.WriteLine("{0} Exception caught!", e);
+
+                    Console.WriteLine("{0} - ошибка {1}!", wb_path, e.GetType().Name);
+                    continue;
+                    //Console.ReadKey();
+                }
+
+                Console.WriteLine("{0} - успешно!", wb_path);
             }
+
 
             XLWorkbook destWB = new XLWorkbook();
             destWB.Worksheets.Add(dt, "Результат");
