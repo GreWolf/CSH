b9459fd1b58ff72cc4d1c79fa580281a7d3907ff
seminozhenko.ss <grey_wolf.88@mail.ru>
2020-07-14 16:01:26 +0300

    Обработка отедельного файла EXCEL перенесена в метод

--- Test/Program.cs
+++ Test/Program.cs
@@ -31,93 +31,19 @@ namespace Test
             dt.Columns.Add("Дебет", System.Type.GetType("System.Double"));
             dt.Columns.Add("Кредит", System.Type.GetType("System.Double"));
 
-            bool ReadMode = false;
-
 
             foreach (var wb_path in paths)
             {
                 try
                 {
-                    using (var excelWorkbook = new XLWorkbook(wb_path))
-                    {
-                        var nonEmptyDataRows = excelWorkbook.Worksheet(1).RowsUsed();
-
-                        int n = 0;
-
-
-                        string OSV, KFO, partner, contract, debet, kredit;
-                        OSV = "";
-                        KFO = "";
-                        partner = "";
-
-                        foreach (var dataRow in nonEmptyDataRows)
-                        {
-
-                            var cell = dataRow.Cell(1);
-
-                            if (cell.Value.ToString() == "Итого")
-                            {
-                                break;
-                            }
-
-                            if (cell.Value.ToString() == "Договоры")
-                            {
-                                ReadMode = true;
-                                continue;
-                            }
-
-                            if (ReadMode == true)
-                            {
-
-                                switch (cell.Style.Alignment.Indent)
-                                {
-                                    case 0:
-                                        OSV = cell.Value.ToString();
-                                        break;
-                                    case 2:
-                                        KFO = cell.Value.ToString();
-                                        break;
-                                    case 4:
-                                        partner = cell.Value.ToString();
-                                        break;
-                                    case 6:
-                                        contract = cell.Value.ToString();
-                                        debet = dataRow.Cell(19).Value.ToString();
-                                        kredit = dataRow.Cell(21).Value.ToString();
-
-                                        if (debet != "" || kredit != "")
-                                        {
-                                            DataRow _row = dt.NewRow();
-
-                                            _row["ОСВ"] = OSV;
-                                            _row["КФО"] = KFO;
-                                            _row["Контрагент"] = partner;
-                                            _row["Договор"] = contract;
-                                            _row["Дебет"] = debet == "" ? 0 : Convert.ToDouble(debet.Replace(".", ","));
-                                            _row["Кредит"] = kredit == "" ? 0 : Convert.ToDouble(kredit.Replace(".", ","));
-
-                                            dt.Rows.Add(_row);
-
-                                            //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", OSV, KFO, partner, contract, debet, kredit);
-                                        }
-
-                                        n += 1;
-
-                                        break;
-                                }
-                            }
-                        }
-                    }
+                    parseExcelFile(wb_path, ref dt);
                 }
 
 
                 catch (System.IO.IOException e)
                 {
-                    //Console.WriteLine("{0} Exception caught!", e);
-
                     Console.WriteLine("{0} - ошибка {1}!", wb_path, e.GetType().Name);
                     continue;
-                    //Console.ReadKey();
                 }
 
                 Console.WriteLine("{0} - успешно!", wb_path);
@@ -130,5 +56,89 @@ namespace Test
             Console.ReadKey();
 
         }
+
+
+        static public void parseExcelFile(string wb_path, ref DataTable dt)
+        {
+            using (var excelWorkbook = new XLWorkbook(wb_path))
+            {
+                var nonEmptyDataRows = excelWorkbook.Worksheet(1).RowsUsed();
+
+                int n = 0;
+
+                string OSV, KFO, partner, contract, debet, kredit;
+
+                OSV = "";
+                KFO = "";
+                partner = "";
+
+                bool ReadMode = false;
+
+                foreach (var dataRow in nonEmptyDataRows)
+                {
+
+                    var cell = dataRow.Cell(1);
+
+                    if (cell.Value.ToString() == "Итого")
+                    {
+                        break;
+                    }
+
+                    if (cell.Value.ToString() == "Договоры")
+                    {
+                        ReadMode = true;
+                        continue;
+                    }
+
+                    if (ReadMode == true)
+                    {
+
+                        switch (cell.Style.Alignment.Indent)
+                        {
+                            case 0:
+                                OSV = cell.Value.ToString();
+                                break;
+
+                            case 2:
+                                KFO = cell.Value.ToString();
+                                break;
+
+                            case 4:
+                                partner = cell.Value.ToString();
+                                break;
+
+                            case 6:
+                                contract = cell.Value.ToString();
+                                debet = dataRow.Cell(19).Value.ToString();
+                                kredit = dataRow.Cell(21).Value.ToString();
+
+                                if (debet != "" || kredit != "")
+                                {
+                                    DataRow _row = dt.NewRow();
+
+                                    _row["ОСВ"] = OSV;
+                                    _row["КФО"] = KFO;
+                                    _row["Контрагент"] = partner;
+                                    _row["Договор"] = contract;
+                                    _row["Дебет"] = debet == "" ? 0 : Convert.ToDouble(debet.Replace(".", ","));
+                                    _row["Кредит"] = kredit == "" ? 0 : Convert.ToDouble(kredit.Replace(".", ","));
+
+                                    dt.Rows.Add(_row);
+
+                                    //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", OSV, KFO, partner, contract, debet, kredit);
+                                }
+
+                                n += 1;
+
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
     }
 }
